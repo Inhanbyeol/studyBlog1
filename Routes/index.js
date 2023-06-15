aabebe69d73e738a2e827fd563481908b0f6e37c
inhanbyeol <inhanbyeol94@gmail.com>
2023-06-16 01:52:47 +0900

    a typing error (find)

--- Routes/index.js
+++ Routes/index.js
@@ -6,7 +6,7 @@ const db = require('../database/library');
 //all read
 app.get('/', async (req, res) => {
   try {
-    res.json((await db.infd('post', {}, { password: 0 })).sort((a, b) => new Date(b.date) - new Date(a.date)));
+    res.json((await db.find('post', {}, { password: 0 })).sort((a, b) => new Date(b.date) - new Date(a.date)));
   } catch (err) {
     return res.json(err);
   }
