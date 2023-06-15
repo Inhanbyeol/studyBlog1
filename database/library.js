d26f3c58bf376604b69a632a0575467675c4c492
inhanbyeol <inhanbyeol94@gmail.com>
2023-06-16 05:01:09 +0900

    [create] postId, createId error patch

--- database/library.js
+++ database/library.js
@@ -80,7 +80,7 @@ module.exports = {
       const db = require(`./Schemas/${collection}`);
       const result = await db.find().sort({ postId: 'desc' }).limit(1);
 
-      return (result.postId ?? 0) + 1;
+      return result.length ? result[0].postId + 1 : 1;
     } catch (err) {
       return err;
     } finally {
@@ -94,7 +94,7 @@ module.exports = {
       const db = require(`./Schemas/${collection}`);
       const result = await db.find().sort({ commentId: 'desc' }).limit(1);
 
-      return (result.commentId ?? 0) + 1;
+      return result.length ? result[0].commentId + 1 : 1;
     } catch (err) {
       return err;
     } finally {
